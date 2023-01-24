d23b9b53a89334375ceced97cfe44ab557d9f373
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2023-01-24 17:14:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
+include("config.php");
+
 function convertTime($ms) {
     return floor($ms/60000) . ':' . str_pad(floor(($ms%60000)/1000), 2, '0', STR_PAD_LEFT) . '.' . str_pad(floor($ms%1000), 3, '0', STR_PAD_LEFT);
 }
-include("config.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,10 +16,16 @@ include("config.php");
   <body>
     <h1>SpeedrunIGT Records</h1>
     <?php
+    $runs = [];
+
     foreach(glob($igtPath . "/records/*.json") as $filename) {
-        $file = file_get_contents($filename);
-        $array = json_decode($file, true);
-        if($array["is_completed"] || (isset($array["timelines"][6]) && $array["timelines"][6]["name"] == "enter_end")) {
+      $file = file_get_contents($filename);
+      $array = json_decode($file, true);
+      if($array["is_completed"] || (isset($array["timelines"][6]) && $array["timelines"][6]["name"] == "enter_end")) {
+        $runs[] = $array;
+      }
+    }
+    foreach($runs as $run) {
      ?>
     <table>
      <tr>
@@ -27,14 +34,13 @@ include("config.php");
         <th>RTA</th>
      </tr>
      <?php
-        echo "<tr><td>is_completed</td><td>" . ($array["is_completed"] ? "true" : "false") . "</td><td>-</td></tr>";
-        echo "<tr><td>final_igt</td><td><strong>" . convertTime($array["final_igt"]) . "</strong></td><td>-</td></tr>";
-        foreach($array["timelines"] as $timeline) {
+        echo "<tr><td>is_completed</td><td>" . ($run["is_completed"] ? "true" : "false") . "</td><td>-</td></tr>";
+        echo "<tr><td>final_igt</td><td><strong>" . convertTime($run["final_igt"]) . "</strong></td><td>-</td></tr>";
+        foreach($run["timelines"] as $timeline) {
             echo "<tr><td>" . $timeline["name"] . "</td><td>" . convertTime($timeline["igt"]) . "</td><td>" .  convertTime($timeline["rta"]) . "</td></tr>";
         }
         echo "</table><hr>";
         }
-    }
     ?>
     </table>
   </body>
