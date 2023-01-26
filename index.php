4e0aed1c865c5b5965aa5acf031eccfed1b5860b
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2023-01-26 21:38:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,24 @@ include("config.php");
 function convertTime($ms) {
   return floor($ms/60000) . ':' . str_pad(floor(($ms%60000)/1000), 2, '0', STR_PAD_LEFT) . '.' . str_pad(floor($ms%1000), 3, '0', STR_PAD_LEFT);
 }
+
+function parseJSON($igtPath) {
+  $runs = [];
+  foreach(glob($igtPath . "/records/*.json") as $filename) {
+    $file = file_get_contents($filename);
+    $array = json_decode($file, true);
+    if($array["is_completed"] || (isset($array["timelines"][6]) && $array["timelines"][6]["name"] == "enter_end")) {
+      $runs[] = $array;
+    }
+  }
+  return $runs;
+}
+
+function sortRuns(&$runs) {
+  $completedRunSort = array_column($runs, 'is_completed');
+  $igtSort = array_column($runs, 'final_igt'); 
+  array_multisort($completedRunSort, SORT_DESC, $igtSort, SORT_ASC, $runs);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,21 +33,11 @@ function convertTime($ms) {
   </head>
   <body>
     <h1>SpeedrunIGT Records</h1>
+
     <?php
-    $runs = [];
-
-    foreach(glob($igtPath . "/records/*.json") as $filename) {
-      $file = file_get_contents($filename);
-      $array = json_decode($file, true);
-      if($array["is_completed"] || (isset($array["timelines"][6]) && $array["timelines"][6]["name"] == "enter_end")) {
-        $runs[] = $array;
-      }
-    }
-
-    $completedRunSort = array_column($runs, 'is_completed');
-    $igtSort = array_column($runs, 'final_igt'); 
-    array_multisort($completedRunSort, SORT_DESC, $igtSort, SORT_ASC, $runs);
-
+    $runs = parseJSON($igtPath);
+    sortRuns($runs);
+    
     foreach($runs as $run) {
      ?>
      <div style="margin: 1rem 0;">
@@ -50,9 +58,13 @@ function convertTime($ms) {
             <td>-</td>
           </tr>
           <?php
-            foreach($run["timelines"] as $timeline) {
-                echo "<tr><td>" . $timeline["name"] . "</td><td>" . convertTime($timeline["igt"]) . "</td><td>" .  convertTime($timeline["rta"]) . "</td></tr>";
-            }
+            foreach($run["timelines"] as $timeline) { ?>
+              <tr>
+                <td><?= $timeline["name"]; ?></td>
+                <td><?= convertTime($timeline["igt"]); ?></td>
+                <td><?= convertTime($timeline["rta"]); ?></td>
+              </tr>
+            <?php }
           ?>
         </table>
         </div>
