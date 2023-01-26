1c9ef73eb4d07b39a836226d05f1dc76a730bc45
reclaimingmytime <35609464+reclaimingmytime@users.noreply.github.com>
2023-01-26 21:34:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -35,16 +35,25 @@ function convertTime($ms) {
      <div style="margin: 1rem 0;">
         <table>
           <tr>
-              <th>Info</th>
-              <th>IGT</th>
-              <th>RTA</th>
+            <th>Info</th>
+            <th>IGT</th>
+            <th>RTA</th>
+          </tr>
+          <tr>
+            <td>is_completed</td>
+            <td><?= $run["is_completed"] ? "true" : "false" ?></td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td><strong>final_igt</strong></td>
+            <td><strong><?= convertTime($run["final_igt"]) ?></strong></td>
+            <td>-</td>
           </tr>
           <?php
-              echo "<tr><td>is_completed</td><td>" . ($run["is_completed"] ? "true" : "false") . "</td><td>-</td></tr>";
-              echo "<tr><td><strong>final_igt</strong></td><td><strong>" . convertTime($run["final_igt"]) . "</strong></td><td>-</td></tr>";
-              foreach($run["timelines"] as $timeline) {
-                  echo "<tr><td>" . $timeline["name"] . "</td><td>" . convertTime($timeline["igt"]) . "</td><td>" .  convertTime($timeline["rta"]) . "</td></tr>";
-              } ?>
+            foreach($run["timelines"] as $timeline) {
+                echo "<tr><td>" . $timeline["name"] . "</td><td>" . convertTime($timeline["igt"]) . "</td><td>" .  convertTime($timeline["rta"]) . "</td></tr>";
+            }
+          ?>
         </table>
         </div>
         <hr>
